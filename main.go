2fda7c8a1d0743b80a421d29319b23b67ff9de22
sxz799 <sxz799@126.com>
2023-03-30 21:59:45 +0800

    1

--- main.go
+++ main.go
@@ -2,70 +2,18 @@ package main
 
 import (
 	"bufio"
-	"bytes"
 	"chatgpt-api-go/model"
-	"encoding/json"
 	"fmt"
-	"net/http"
-	"net/url"
 	"os"
 	"strings"
 	"sync"
-	"time"
 )
-
-func SendChatPostMsg(msgs []model.Message, conf model.ApiConfig) string {
-	reqData := model.Request{
-		Model:    conf.Model,
-		Messages: msgs,
-	}
-
-	// 将请求数据编码为 JSON 格式
-	reqBody, err := json.Marshal(reqData)
-	if err != nil {
-		panic(err)
-	}
-
-	// 创建 HTTP 请求
-	req, err := http.NewRequest("POST", "https://api.openai.com/v1/chat/completions", bytes.NewBuffer(reqBody))
-	if err != nil {
-		panic(err)
-	}
-
-	// 设置请求头
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Authorization", "Bearer "+conf.ApiKey)
-
-	// 发送请求
-	proxyUrl, err := url.Parse(conf.ProxyUrl)
-	if err != nil {
-		panic(err)
-	}
-	client := &http.Client{
-		Timeout: 30 * time.Second,
-		Transport: &http.Transport{
-			Proxy: http.ProxyURL(proxyUrl),
-		},
-	}
-	resp, err := client.Do(req)
-	if err != nil {
-		return "发生了错误:" + err.Error()
-	}
-	defer resp.Body.Close()
-
-	// 解析响应数据
-	var respData model.ChatCompletion
-	if err := json.NewDecoder(resp.Body).Decode(&respData); err != nil {
-		panic(err)
-	}
-	return respData.Choices[0].Message.Role + ":" + respData.Choices[0].Message.Content
-}
 
 func main() {
 	// 准备请求数据
 	conf := model.ApiConfig{
 		Model:    "gpt-3.5-turbo",
-		ApiKey:   "sk-xxx",
+		ApiKey:   "sk-",
 		ProxyUrl: "http://127.0.0.1:7890",
 	}
 	var wg sync.WaitGroup
@@ -83,7 +31,7 @@ func main() {
 				Role:    "user",
 				Content: question,
 			})
-			fmt.Println(SendChatPostMsg(msgs, conf))
+			fmt.Println(model.SendChatPostMsg(msgs, conf))
 			wg.Done()
 		}()
 		wg.Wait()
--- model/model.go
+++ model/model.go
@@ -1,5 +1,13 @@
 package model
 
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/url"
+	"time"
+)
+
 type Request struct {
 	Model    string    `json:"model"`
 	Messages []Message `json:"messages"`
@@ -35,4 +43,51 @@ type ApiConfig struct {
 	Model    string
 	ApiKey   string
 	ProxyUrl string
+}
+
+func SendChatPostMsg(msgs []Message, conf ApiConfig) string {
+	reqData := Request{
+		Model:    conf.Model,
+		Messages: msgs,
+	}
+
+	// 将请求数据编码为 JSON 格式
+	reqBody, err := json.Marshal(reqData)
+	if err != nil {
+		panic(err)
+	}
+
+	// 创建 HTTP 请求
+	req, err := http.NewRequest("POST", "https://api.openai.com/v1/chat/completions", bytes.NewBuffer(reqBody))
+	if err != nil {
+		panic(err)
+	}
+
+	// 设置请求头
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer "+conf.ApiKey)
+
+	// 发送请求
+	proxyUrl, err := url.Parse(conf.ProxyUrl)
+	if err != nil {
+		panic(err)
+	}
+	client := &http.Client{
+		Timeout: 30 * time.Second,
+		Transport: &http.Transport{
+			Proxy: http.ProxyURL(proxyUrl),
+		},
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		return "发生了错误:" + err.Error()
+	}
+	defer resp.Body.Close()
+
+	// 解析响应数据
+	var respData ChatCompletion
+	if err := json.NewDecoder(resp.Body).Decode(&respData); err != nil {
+		panic(err)
+	}
+	return respData.Choices[0].Message.Role + ":" + respData.Choices[0].Message.Content
 }
