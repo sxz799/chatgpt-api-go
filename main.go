175c1def6a3e101fbbc0f2b28c03fdea45b0ee10
sxz799 <sxz799@126.com>
2023-08-26 19:18:31 +0800

    优化配置选项 添加历史内容长度

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"chatgpt-api-go/model"
 	"fmt"
 	"os"
-	"strings"
 	"sync"
 )
 
@@ -13,7 +12,7 @@ func main() {
 	// 准备请求数据
 	conf := model.ApiConfig{
 		Model:         "gpt-3.5-turbo",
-		ApiKey:        "1pk-this-is-a-real-free-pool-token-for-everyone",
+		ApiKey:        "pk-this-is-a-real-free-pool-token-for-everyone",
 		ApiServer:     "https://ai.fakeopen.com/v1/chat/completions",
 		ProxyUrl:      "",
 		HistoryNumber: 6,
@@ -25,27 +24,36 @@ func main() {
 		Content: "你好！",
 	})
 	result, err := model.SendChatPostMsg(msgs, conf)
-	fmt.Println(result)
+	fmt.Println(result + "(您可以输入三个空格以开始一段新的对话！)")
 	if err != nil {
 		return
 	}
 	for {
 		wg.Add(1)
+		fmt.Println("请继续提问: ")
 		go func() {
 			reader := bufio.NewReader(os.Stdin)
 			question, _ := reader.ReadString('\n')
-			if strings.EqualFold(question, "重新开始") {
+			if question == "   \n" {
 				msgs = make([]model.Message, 0)
+				fmt.Println("好的，现在重新开始对话！ ")
+				wg.Done()
+				return
+			} else {
+				msgs = append(msgs, model.Message{
+					Role:    "user",
+					Content: question,
+				})
 			}
-			msgs = append(msgs, model.Message{
-				Role:    "user",
-				Content: question,
-			})
-			fmt.Println(model.SendChatPostMsg(msgs, conf))
+
+			msg, tErr := model.SendChatPostMsg(msgs, conf)
+			if tErr != nil {
+				fmt.Println(tErr.Error())
+			}
+			fmt.Println(msg)
 			wg.Done()
 		}()
 		wg.Wait()
-		fmt.Println("请继续提问: ")
 	}
 
 }
